f4b7e5b1e776ffb9f665a7e84bbbac44f9d98aa7
Slowcobo <spencer.coleman92@gmail.com>
2020-07-10 22:18:28 -0700

    Add bugs and sea

--- src/CritterApp.js
+++ src/CritterApp.js
@@ -4,17 +4,18 @@ import CritterList from "./CritterList";
 import CritterInfo from "./CritterInfo";
 
 export default function CritterApp() {
+  const [mode, setMode] = useState("fish");
   const [critters, setCritters] = useState([]);
   const [currentCritter, setCurrentCritter] = useState();
   const [showCritter, setShowCritter] = useState(false);
 
   useEffect(() => {
     const getData = async () => {
-      const response = await axios.get("http://acnhapi.com/v1a/fish/");
+      const response = await axios.get(`http://acnhapi.com/v1a/${mode}/`);
       setCritters(response.data);
     };
     getData();
-  }, []);
+  }, [mode]);
 
   const getCritter = (critterId) => {
     setCurrentCritter(critters[critterId - 1]);
@@ -38,8 +39,10 @@ export default function CritterApp() {
           closeCritter={closeCritter}
         />
       )}
-
       {/* Show list of current critters */}
+      <button onClick={() => setMode("fish")}>Fish</button>
+      <button onClick={() => setMode("sea")}>Sea</button>
+      <button onClick={() => setMode("bugs")}>Bugs</button>
       <CritterList critters={critters} getCritter={getCritter} />
     </>
   );
