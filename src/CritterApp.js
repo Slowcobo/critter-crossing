df056e50ce86588168b1edcc7826b83bc3785d51
Slowcobo <spencer.coleman92@gmail.com>
2020-07-15 00:56:51 -0700

    Add date context and CritterOptions

--- src/CritterApp.js
+++ src/CritterApp.js
@@ -1,31 +1,13 @@
 import React, { useState, useEffect, useContext } from "react";
-import { makeStyles } from "@material-ui/core/styles";
 import axios from "axios";
 import Grid from "@material-ui/core/Grid";
-import InputLabel from "@material-ui/core/InputLabel";
-import MenuItem from "@material-ui/core/MenuItem";
-import FormControl from "@material-ui/core/FormControl";
-import Select from "@material-ui/core/Select";
-import Typography from "@material-ui/core/Typography";
-import Slider from "@material-ui/core/Slider";
 import { CritterContext } from "./contexts/CritterContext";
+import CritterOptions from "./CritterOptions";
 import CritterList from "./CritterList";
 import CritterInfo from "./CritterInfo";
 
-const useStyles = makeStyles((theme) => ({
-  formControl: {
-    margin: theme.spacing(1),
-    minWidth: 120,
-  },
-  selectEmpty: {
-    marginTop: theme.spacing(2),
-  },
-}));
-
 export default function CritterApp() {
-  const { critterType } = useContext(CritterContext);
-  const classes = useStyles();
-  const [date, setDate] = useState({ month: "", time: "" });
+  const { critterType, date } = useContext(CritterContext);
   const [critters, setCritters] = useState({ bugs: [], fish: [], sea: [] });
   const [currentCritter, setCurrentCritter] = useState();
   const [showCritterInfo, setShowCritterInfo] = useState(false);
@@ -45,13 +27,6 @@ export default function CritterApp() {
     };
     //Get critter data from API
     getCritterData();
-
-    //Set date to current
-    const d = new Date();
-    setDate({
-      month: d.getMonth() + 1,
-      time: d.getHours(),
-    });
   }, []);
 
   const getCritter = (critterId) => {
@@ -63,20 +38,6 @@ export default function CritterApp() {
     setCurrentCritter();
     setShowCritterInfo(false);
   };
-
-  const handleMonthChange = (event) => {
-    const newDate = { ...date, month: event.target.value };
-    setDate(newDate);
-  };
-
-  const handleTimeChange = (event, value) => {
-    const newDate = { ...date, time: value };
-    setDate(newDate);
-  };
-
-  function valuetext(value) {
-    return `${value}`;
-  }
 
   return (
     <Grid container>
@@ -94,44 +55,7 @@ export default function CritterApp() {
       {/* Month Selection */}
       <Grid container item justify="center">
         <Grid item xs={11} md={6}>
-          <FormControl className={classes.formControl}>
-            <InputLabel id="month-selector">Month</InputLabel>
-            <Select
-              labelId="month-selector"
-              id="month-selector"
-              value={date.month}
-              onChange={handleMonthChange}
-            >
-              <MenuItem value={1}>January</MenuItem>
-              <MenuItem value={2}>February</MenuItem>
-              <MenuItem value={3}>March</MenuItem>
-              <MenuItem value={4}>April</MenuItem>
-              <MenuItem value={5}>May</MenuItem>
-              <MenuItem value={6}>June</MenuItem>
-              <MenuItem value={7}>July</MenuItem>
-              <MenuItem value={8}>August</MenuItem>
-              <MenuItem value={9}>September</MenuItem>
-              <MenuItem value={10}>October</MenuItem>
-              <MenuItem value={11}>November</MenuItem>
-              <MenuItem value={12}>December</MenuItem>
-            </Select>
-          </FormControl>
-
-          {/* Time Slider */}
-          <Typography id="discrete-slider" gutterBottom>
-            Time
-          </Typography>
-          <Slider
-            value={Number(date.time)}
-            getAriaValueText={valuetext}
-            aria-labelledby="discrete-slider"
-            valueLabelDisplay="auto"
-            step={1}
-            marks
-            min={0}
-            max={23}
-            onChange={handleTimeChange}
-          />
+          <CritterOptions />
         </Grid>
       </Grid>
 
--- src/contexts/CritterContext.js
+++ src/contexts/CritterContext.js
@@ -4,10 +4,14 @@ export const CritterContext = createContext();
 
 export function CritterProvider(props) {
   const [critterType, setCritterType] = useState("bugs");
+  const [date, setDate] = useState({ month: 1, time: 0 });
   const changeCritterType = (type) => setCritterType(type);
+  const changeDate = (date) => setDate(date);
 
   return (
-    <CritterContext.Provider value={{ critterType, changeCritterType }}>
+    <CritterContext.Provider
+      value={{ critterType, changeCritterType, date, changeDate }}
+    >
       {props.children}
     </CritterContext.Provider>
   );
