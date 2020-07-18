a7c6bc5aa1b08ceb11af889f9d24c5e3a216f926
Slowcobo <spencer.coleman92@gmail.com>
2020-07-17 17:16:46 -0700

    Add CritterCollection and Toggle

--- src/CritterApp.js
+++ src/CritterApp.js
@@ -1,16 +1,19 @@
 import React, { useState, useEffect, useContext } from "react";
 import axios from "axios";
 import Grid from "@material-ui/core/Grid";
+import Switch from "@material-ui/core/Switch";
 import { CritterContext } from "./contexts/CritterContext";
 import CritterOptions from "./CritterOptions";
 import CritterList from "./CritterList";
 import CritterInfo from "./CritterInfo";
+import CritterCollection from "./CritterCollection";
 
 export default function CritterApp() {
   const { critterType, date } = useContext(CritterContext);
   const [critters, setCritters] = useState({ bugs: [], fish: [], sea: [] });
   const [currentCritter, setCurrentCritter] = useState();
   const [showCritterInfo, setShowCritterInfo] = useState(false);
+  const [showCollection, setShowCollection] = useState(false);
 
   useEffect(() => {
     const getCritterData = async () => {
@@ -39,6 +42,10 @@ export default function CritterApp() {
     setShowCritterInfo(false);
   };
 
+  const handleChange = () => {
+    setShowCollection(!showCollection);
+  };
+
   return (
     <Grid container>
       {/* Only render critter info if currentCritter has been selected */}
@@ -59,14 +66,32 @@ export default function CritterApp() {
         </Grid>
       </Grid>
 
-      {/* List of current critters */}
+      {/* Toggle List and Collection */}
       <Grid container item justify="center">
         <Grid item xs={12} md={6}>
-          <CritterList
-            critters={critters[critterType]}
-            date={date}
-            getCritter={getCritter}
+          <Switch
+            checked={showCollection}
+            onChange={handleChange}
+            color="primary"
+            name="checkedB"
+            inputProps={{ "aria-label": "primary checkbox" }}
           />
+          Show Collection
+        </Grid>
+      </Grid>
+
+      {/* List or Collection of current critters */}
+      <Grid container item justify="center">
+        <Grid item xs={12} md={6}>
+          {showCollection ? (
+            <CritterCollection />
+          ) : (
+            <CritterList
+              critters={critters[critterType]}
+              date={date}
+              getCritter={getCritter}
+            />
+          )}
         </Grid>
       </Grid>
     </Grid>
---
+++ src/CritterCollection.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function CritterCollection() {
+  return <div>Critter Collection will go here</div>;
+}
